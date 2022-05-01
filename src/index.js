dffcabaeeeefb3e874d114947a4998654b376528
Carlos Serrano <carlos.pastor@mailonline.co.uk>
2022-05-01 17:47:11 +0200

    feat: add scroll top button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+import debounce from "lodash.debounce";
 import "./styles.css";
+
+const init = () => {
+  const scrollTopBtn = document.querySelector(".scroll-top-btn");
+
+  if (scrollTopBtn) {
+    scrollTopBtn.addEventListener("click", () => {
+      window.scroll({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+
+    document.addEventListener(
+      "scroll",
+      debounce(() => {
+        if (window.scrollY > window.innerHeight) {
+          scrollTopBtn.classList.add("active");
+        } else if (scrollTopBtn.classList.contains("active")) {
+          scrollTopBtn.classList.remove("active");
+        }
+      }, 150)
+    );
+  }
+};
+
+if (document.readyState === "complete") {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init);
+}
